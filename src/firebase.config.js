f8ea561f88f7b72dfe8278f032d11c8a4378731c
Nmatther <nmatther@icloud.com>
2024-04-17 15:34:49 -0700

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1cqVvn8TPJbUpwFebvr6NIQVhrvQOdrM",
+  authDomain: "clone-1cd10.firebaseapp.com",
+  projectId: "clone-1cd10",
+  storageBucket: "clone-1cd10.appspot.com",
+  messagingSenderId: "346989205349",
+  appId: "1:346989205349:web:db6ad9455ee9f61ebdb38c",
+  measurementId: "G-5KVNN8R4LR"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
+
+
