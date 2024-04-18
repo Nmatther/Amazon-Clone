eadbda5356e00c4dcf5e988046c4f0ad31493c50
Nmatther <nmatther@icloud.com>
2024-04-17 17:32:54 -0700

    updated signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { RotatingLines } from "react-loader-spinner";
 import { darkLogo } from "../assets/index";
 
 const Signin = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errEmail, setErrEmail] = useState("");
   const [errPassword, setErrPassword] = useState("");
   // Firebase Error
-
+  const [userEmailErr, setUserEmailErr] = useState("");
+  const [userPassErr, setUserPassErr] = useState("");
   // Loading State start here
-
+  const [loading, setLoading] = useState(false);
+  const [successMsg, setSuccessMsg] = useState("");
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -32,7 +38,27 @@ const Signin = () => {
       setErrPassword("Enter your password");
     }
     if (email && password) {
-      console.log(email,password)
+      setLoading(true)
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+        // ...
+        setLoading(false)
+          setSuccessMsg("Logged in Successfully! Welcome Back!");
+          setTimeout(() =>{
+            navigate("/")
+          }, 2000)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          if (errorCode.includes("auth/invalid-email")) {
+            setUserEmailErr("Invalid Email");
+          }
+          if (errorCode.includes("auth/wrong-password")) {
+            setUserPassErr("Wrong Password! Try Again.");
+          }
+        });
       setEmail("")
       setPassword("")
     }
@@ -40,7 +66,14 @@ const Signin = () => {
   return (
     <div className="w-full">
       <div className="w-full bg-gray-100 pb-10">     
-          <form className="w-[350px] mx-auto flex flex-col items-center">
+          {
+            successMsg ? (
+              <div className="w-full flex justify-center items-center py-32">
+                <p className="border-[1px] border-green-600 text-green-500 font-titleFont text-lg font-semibold px-6 py-2">
+                  {successMsg}
+                </p>
+              </div>
+            ) : (<form className="w-[350px] mx-auto flex flex-col items-center">
             <Link to="/">
               <img className="w-32" src={darkLogo} alt="darkLogo" />
             </Link>
@@ -59,12 +92,12 @@ const Signin = () => {
                     className="w-full lowercase py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput duration-100"
                     type="email"
                   />
-                  {errEmail && (
+                  {userEmailErr && (
                     <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
                       <span className="italic font-titleFont font-extrabold text-base">
                         !
                       </span>
-                      {errEmail}
+                      {userEmailErr}
                     </p>
                   )}
                  
@@ -77,12 +110,12 @@ const Signin = () => {
                     className="w-full lowercase py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput duration-100"
                     type="password"
                   />
-                  {errPassword && (
+                  {userPassErr && (
                     <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
                       <span className="italic font-titleFont font-extrabold text-base">
                         !
                       </span>
-                      {errPassword}
+                      {userPassErr}
                     </p>
                   )}
                  
@@ -93,7 +126,18 @@ const Signin = () => {
                 >
                   Continue
                 </button>
-               
+                {loading && (
+                  <div className="flex justify-center">
+                    <RotatingLines
+                      visible={true}
+                      height="50"
+                      width="50"
+                      color="#fe"
+                      strokeWidth="5"
+                      animationDuration="0.75"
+                      />
+                  </div>
+                )}
               </div>
               <p className="text-xs text-black leading-4 mt-4">
                 By Continuing, you agree to Amazon's{" "}
@@ -117,7 +161,8 @@ const Signin = () => {
                 Create your Amazon account
               </button>
             </Link>
-          </form>
+          </form>)
+          }
       
       </div>
       <div className="w-full bg-gradient-to-t from-white via-white to-zinc-200 flex flex-col gap-4 justify-center items-center py-10">
